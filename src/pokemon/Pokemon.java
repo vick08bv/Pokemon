f398977d66477778f1b97ceee5ea2b4d1e30fe27
vick08bv <>
2019-08-23 22:49:12 -0500

    Prueba de la clase Pokemon

--- src/pokemon/Pokemon.java
+++ src/pokemon/Pokemon.java
@@ -1,19 +1,69 @@
 package pokemon;
 
-//@author Méndez Oliveros Victor Hugo
-
+/**
+ * Implementación de un pokemón.
+ * @author Méndez Oliveros Victor Hugo.
+ * @author Mercado Ayala Arturo.
+ */
 public class Pokemon {
 
+    /**
+     * Número del pokemón.
+     */
     private int id;
-    private String nombre; 
-    private String tipo; 
-    private int vida;
-    private int ataque;
-    private int defensa;
-    private int velocidad;
-    private int generacion;
-    private boolean legendario;
     
+    /**
+     * Nombre del pokemón.
+     */
+    private String nombre;
+    
+    /**
+     * Tipo de pokemón.
+     */
+    private String tipo;
+    
+    /**
+     * Cantidad de vida que posee.
+     */
+    private int vida;
+    
+    /**
+     * Potencia de su ataque.
+     */
+    private int ataque;
+    
+    /**
+     * Capacidad para defenderse.
+     */
+    private int defensa;
+    
+    /**
+     * Velocidad que posee.
+     */
+    private int velocidad;
+    
+    /**
+     * Generación a la que pertenece.
+     */
+    private int generacion;
+    
+    /**
+     *true si el pokemón es legendario.
+     */
+    private boolean legendario;
+
+    /**
+     * Cosntructor con los datos que llevará el pokemón.
+     * @param id número de pokemón.
+     * @param nombre Nombre.
+     * @param tipo Tipo.
+     * @param vida Vida.
+     * @param ataque Ataque.
+     * @param defensa Defensa.
+     * @param velocidad Velocidad.
+     * @param generacion Generación.
+     * @param legendario Legendario.
+     */
     public Pokemon( int id, String nombre, String tipo, 
                     int vida, int ataque, int defensa,
                     int velocidad, int generacion, 
@@ -30,79 +80,159 @@ public class Pokemon {
         this.legendario = legendario;
     
     }
-
-    public void setId(int id) {
-        this.id = id;
-    }
-
-    public void setNombre(String nombre) {
-        this.nombre = nombre;
-    }
-
-    public void setTipo(String tipo) {
-        this.tipo = tipo;
-    }
-
-    public void setVida(int vida) {
-        this.vida = vida;
-    }
-
-    public void setAtaque(int ataque) {
-        this.ataque = ataque;
-    }
-
-    public void setDefensa(int defensa) {
-        this.defensa = defensa;
-    }
-
-    public void setVelocidad(int velocidad) {
-        this.velocidad = velocidad;
-    }
-
-    public void setGeneracion(int generacion) {
-        this.generacion = generacion;
-    }
-
-    public void setLegendario(boolean legendario) {
-        this.legendario = legendario;
-    }
-
-    public int getId() {
-        return id;
-    }
-
-    public String getNombre() {
-        return nombre;
-    }
-
-    public String getTipo() {
-        return tipo;
-    }
-
-    public int getVida() {
-        return vida;
-    }
-
-    public int getAtaque() {
-        return ataque;
-    }
-
-    public int getDefensa() {
-        return defensa;
-    }
-
-    public int getVelocidad() {
-        return velocidad;
-    }
-
-    public int getGeneracion() {
-        return generacion;
-    }
-
-    public boolean isLegendario() {
-        return legendario;
+    
+    /**
+     * DEvuelve el id del pokemón.
+     * @return id.
+     */
+    public int getId(){
+    return id;
     }
     
+    /**
+     * Devuelve el nombre del pokemón.
+     * @return nombre
+     */
+    public String getNombre(){
+    return nombre;
+    }
+    
+    /**
+     * Devuelve el tipo de pokemón.
+     * @return tipo
+     */
+    public String getTipo(){
+    return tipo;
+    }
+    
+    /**
+     * Devuelve la cantida de vida del pokemón.
+     * @return vida
+     */
+    public int getVida(){
+    return vida;
+    }
+    
+    /**
+     * Devuelve el ataque del pokemón.
+     * @return ataque
+     */
+    public int getAtaque(){
+    return ataque;
+    }
+    
+    /**
+     * Devuelve la defensa del pokemón.
+     * @return defensa
+     */
+    public int getDefensa(){
+    return defensa;
+    }
+    
+    /**
+     * Devuelve la velocidad del pokemón.
+     * @return velocidad
+     */
+    public int getVelocidad(){
+    return velocidad;
+    }
+    
+    /**
+     * Devuelve la generación del pokemón.
+     * @return generacion
+     */
+    public int getGeneracion(){
+    return generacion;
+    }
+    
+    /**
+     * Devuelve true si el pokemón es legendario.
+     * @return legendario.
+     */
+    public boolean getLegendario(){
+    return legendario;
+    }
+    
+    /**
+     * Nuevo id del pokemón.
+     * @param id.
+     */
+    public void setId(int id){
+    this.id = id;
+    }
+    
+    /**
+     * Nuevo nombre del pokemón.
+     * @param nombre
+     */
+    public void setNombre(String nombre){
+    this.nombre = nombre;
+    }
+    
+    /**
+     * Nuevo tipo de pokemón.
+     * @param tipo
+     */
+    public void setTipo(String tipo){
+    this.tipo = tipo;
+    }
+    
+    /**
+     * Asigna una cantidad de vida al pokemón.
+     * @param vida
+     */
+    public void setVida(int vida){
+    this.vida = vida; 
+    }
+    
+    /**
+     * Asigna su ataque al pokemón.
+     * @param ataque
+     */
+    public void setAtaque(int ataque){
+    this.ataque = ataque;
+    }
+    
+    /**
+     * Asigna su defensa al pokemón.
+     * @param defensa
+     */
+    public void setDefensa(int defensa){
+    this.defensa = defensa;
+    }
+    
+    /**
+     * Asigna una velocidad al pokemón.
+     * @param velocidad
+     */
+    public void setVelocidad(int velocidad){
+    this.velocidad = velocidad;
+    }
+    
+    /**
+     * Asigna la generación al pokemón.
+     * @param generacion
+     */
+    public void setGeneracion(int generacion){
+    this.generacion = generacion;
+    }
+    
+    /**
+     * Asigna el valor de legendario al pokemón.
+     * @param legendario
+     */
+    public void setLegendario(boolean legendario){
+    this.legendario = legendario;
+    }
+    
+    /**
+     * Devuelve true si y sólo sí los 
+     * pokemónes a comparar tienen 
+     * los mismos atributos.
+     * @param pokemon Pokemón a ser comparado 
+     * con el pokemón en cuestión.
+     * @return booleano
+     */
     public boolean equals(Pokemon pokemon){
     
         return ((this.id == pokemon.id)&&
@@ -113,28 +243,30 @@ public class Pokemon {
                 (this.defensa == pokemon.defensa) &&
                 (this.velocidad == pokemon.velocidad)&&
                 (this.generacion == pokemon.generacion)&&
-                (this.legendario && pokemon.legendario));
+                (this.legendario == pokemon.legendario));
         
     }
     
+    /**
+     * Información del pokemón.
+     * @return Cadena con información.
+     */
     @Override
     public String toString(){
         
-        String leg = "";
+        String info = "****************************** \n\t"  
+                + id + "  " + nombre +"\n"
+                + "******************************"
+                + "\n TIPO: " + tipo
+                + "\n VIDA: " + vida
+                + "\n ATAQUE: " + ataque
+                + "\n DEFENSA: " + defensa
+                + "\n VELOCIDAD: " + velocidad
+                + "\n GENERACION: " + generacion
+                + "\n LEGENDARIO: " + legendario
+                +"\n------------------------------- \n\n" ;
         
-        if (this.legendario){
-            leg = "Sí";
-        } else {
-            leg = "No";
-        }
-    
-        return String.format("\nId: %s \nNombre: %s \nTipo: %s "
-                           + "\nVida: %s \nAtaque: %s"
-                           + "\nDefensa: %s \nVelocidad: %s "
-                           + "\nGeneracion: %s \nLegendario: ", 
-                             this.id, this.nombre, this.tipo, 
-                             this.vida, this.ataque, this.defensa, 
-                             this.velocidad, this.generacion) + leg;
-        
+        return info;
     }
+    
 }
--- src/pokemon/PruebaPokemon.java
+++ src/pokemon/PruebaPokemon.java
@@ -1,22 +1,80 @@
 package pokemon;
+import java.util.Scanner;
 
-// @author Méndez Oliveros Victor Hugo
-
+/**
+ * Prueba de la clase pokemón.
+ * @author Méndez Oliveros Victor Hugo.
+ * @author Mercado Ayala Arturo.
+ */
 public class PruebaPokemon {
     
-    public static void main(String[] args){
-    
-        Pokemon[] pokedex = new Pokemon[5];
-    
+    /**
+     * Prueba.
+     * @param args 
+     */
+    public static void main(String [] args){
+        
+        Scanner scanner = new Scanner(System.in);
+        
+        //Creación de variables para guardar los atributos necesarios.
         int id;
-        String nombre; 
-        String tipo; 
+        String nombre;
+        String tipo;
         int vida;
         int ataque;
         int defensa;
         int velocidad;
         int generacion;
-        boolean legendario;
+        boolean legendario;  
+        
+        //Creación de un arreglo para guardar los pokemónes en él.
+        Pokemon pokedex [] = new Pokemon [5];
+    
+        /*A cada posición del arreglo le guardamos un pokemón creado
+          con los datos ingresados.*/
+        for(int i = 0 ; i < pokedex.length ; i++){
+        
+            System.out.printf("\nIngresa los datos del "
+                            + "pokemón número %s", i+1);
+        
+            System.out.printf("\nIngresa su id: ");
+            id = scanner.nextInt();
+        
+            System.out.printf("\nIngresa su nombre: ");
+            nombre = scanner.next();
+        
+            System.out.printf("\nIngresa su tipo: ");
+            tipo = scanner.next();
+        
+            System.out.printf("\nIngresa su vida: ");
+            vida = scanner.nextInt();
+        
+            System.out.printf("\nIngresa su ataque: ");
+            ataque = scanner.nextInt();
+        
+            System.out.printf("\nIngresa su defensa: ");
+            defensa = scanner.nextInt();
+        
+            System.out.printf("\nIngresa su velocidad: ");
+            velocidad = scanner.nextInt();
+        
+            System.out.printf("\nIngresa su generación: ");
+            generacion = scanner.nextInt();
+        
+            System.out.printf("\nIngresa si el pokemón es legendario: ");
+            legendario = scanner.nextBoolean();
+        
+            pokedex [i] = new Pokemon(id, nombre, tipo, vida, ataque, 
+                        defensa, velocidad,generacion,legendario); 
+        
+            }
+    
+        //Impresión de cada pokemón en el pokedex.
+        for (Pokemon pokemon : pokedex) {
+            
+            System.out.print(pokemon);
+            
+        }
     
     }
     
